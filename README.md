3fcee349346ce8bdaa18e57863dd4c62d54abd4b
caoyu <yalecyu@gmail.com>
2017-07-29 16:59:55 +0800

    init code and project

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# crnn.caffe
+# Convolutional Recurrent Neural Network
+
+​This software implements the Convolutional Recurrent Neural Network (CRNN) in caffe. Origin software could be found in [crnn](https://github.com/bgshih/crnn) .You can also find the [cnn.pytorch](https://github.com/meijieru/crnn.pytorch) 
+
+# Crnn.caffe
+
+​The initial environment for this project is [warpctc-caffe](https://github.com/xmfbit/warpctc-caffe). You can build a software environment from "warpctc-caffe" project.The author in caffe integrated ctc-loss part, and add some layers, making lstm-ctc can be used in caffe.
+
+​The network framework is from  [cnn.pytorch](https://github.com/meijieru/crnn.pytorch) ,it also can be read from  [crnn](https://github.com/bgshih/crnn) .We have the same network.
+
+​CNN-LSTM-CTC, CNN can get BxC feature vectors,they are as the input of LSTM,LTSM can work the vertors in time_step.From CRNN you can get better way to work as the end-to-end character recognition.At first,I just use the LSTM-CTC( [warpctc-caffe](https://github.com/xmfbit/warpctc-caffe) ) to work with the license plate recognition.Loss is very slow convergence and it always overfitting.The result don't like the [cnn.pytorch](https://github.com/meijieru/crnn.pytorch) ,I run the crnn.pytorch to work with these license plate.Finally,I am determined to write the crnn.caffe.The caffe environment is easier to make a product.
+
+# Build
+
+clone the code
+
+> git clone git@github.com:yalecyu/crnn.caffe.git
+>
+> cd crnn.caffe
+>
+> make
+
+If there some problems,you can confirm your basic the setting of caffe and server.Build this project is the same way like build caffe.
+
+# Test
+
+I will ...
+
+# Train
+
+will
+
+# Acknowledgements
+
+The authors would like to thank the developers of  [crnn](https://github.com/bgshih/crnn)  [cnn.pytorch](https://github.com/meijieru/crnn.pytorch)  [warpctc-caffe](https://github.com/xmfbit/warpctc-caffe),without your help ,I can't write the crnn.caffe.When I send the email to you,always get the suggest quickly.
+
+Please let me know if you encounter any issues.
+
+
---
+++ examples/cpp_recognition/recognition.cpp
@@ -0,0 +1,130 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include "boost/algorithm/string.hpp"
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <utility>
+#include <iosfwd>
+#include <string>
+#include <iostream>
+#include <caffe/caffe.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace caffe;
+using namespace std;
+using namespace cv;
+class Classifier {
+ public:
+  Classifier(const string& model_file,const string& trained_file);
+  std::vector<int> Classify(const cv::Mat& img, int N = 1);
+ private:
+  std::vector<int> Predict(const cv::Mat& img);
+  void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+  void Preprocess(const cv::Mat& img,std::vector<cv::Mat>* input_channels);
+ private:
+  boost::shared_ptr<Net<float> > net_;
+  cv::Size input_geometry_;
+  int num_channels_;
+};
+Classifier::Classifier(const string& model_file,const string& trained_file) {
+/*#ifdef CPU_ONLY
+  Caffe::set_mode(Caffe::CPU);
+#else
+  Caffe::set_mode(Caffe::GPU);
+#endif*/
+  /* Load the network. */
+  net_.reset(new Net<float>(model_file, caffe::TEST));
+  net_->CopyTrainedLayersFrom(trained_file);
+
+  CHECK_EQ(net_->num_inputs(), 1) << "Network should have exactly one input.";
+  CHECK_EQ(net_->num_outputs(), 1) << "Network should have exactly one output.";
+
+  Blob<float>* input_layer = net_->input_blobs()[0];
+  num_channels_ = input_layer->channels();
+  CHECK(num_channels_ == 3 || num_channels_ == 1)
+    << "Input layer should have 1 or 3 channels.";
+  input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
+
+//Blob<float>* output_layer = net_->output_blobs()[0];
+}
+/* Return the top N predictions. */
+std::vector<int> Classifier::Classify(const cv::Mat& img, int N) {
+  std::vector<int> output = Predict(img);
+  return output;
+}
+std::vector<int> Classifier::Predict(const cv::Mat& img) {
+  Blob<float>* input_layer = net_->input_blobs()[0];
+  input_layer->Reshape(1, num_channels_,input_geometry_.height, input_geometry_.width);
+  /* Forward dimension change to all layers. */
+  net_->Reshape();
+  std::vector<cv::Mat> input_channels;
+  WrapInputLayer(&input_channels);
+  Preprocess(img, &input_channels);
+  net_->Forward();
+  /* Copy the output layer to a std::vector */
+  Blob<float>* output_layer = net_->output_blobs()[0];
+  int alphabet_size=output_layer->shape(2);
+  int time_step=output_layer->shape(0);
+  vector<int>pred_label_seq_with_blank(time_step);
+  const float* begin = output_layer->cpu_data();
+  for(int t=0;t<time_step;t++){
+    pred_label_seq_with_blank[t]=max_element(begin,begin+alphabet_size)-begin;
+    begin+=alphabet_size;
+  }
+  return pred_label_seq_with_blank;
+}
+void Classifier::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
+  Blob<float>* input_layer = net_->input_blobs()[0];
+  int width = input_layer->width();
+  int height = input_layer->height();
+  float* input_data = input_layer->mutable_cpu_data();
+  for (int i = 0; i < input_layer->channels(); ++i) {
+    cv::Mat channel(height, width, CV_32FC1, input_data);
+    input_channels->push_back(channel);
+    input_data += width * height;
+  }
+}
+
+void Classifier::Preprocess(const cv::Mat& img,std::vector<cv::Mat>* input_channels) {
+  /* Convert the input image to the input image format of the network. */
+  cv::Mat sample;
+  if (img.channels() == 3 && num_channels_ == 1)
+    cv::cvtColor(img, sample, cv::COLOR_BGR2GRAY);
+  else if (img.channels() == 4 && num_channels_ == 1)
+    cv::cvtColor(img, sample, cv::COLOR_BGRA2GRAY);
+  else if (img.channels() == 4 && num_channels_ == 3)
+    cv::cvtColor(img, sample, cv::COLOR_BGRA2BGR);
+  else if (img.channels() == 1 && num_channels_ == 3)
+    cv::cvtColor(img, sample, cv::COLOR_GRAY2BGR);
+  else
+    sample = img;
+  cv::Mat sample_resized;
+  if (sample.size() != input_geometry_)cv::resize(sample, sample_resized, input_geometry_);
+  else sample_resized = sample;
+  cv::Mat sample_float;
+  if (num_channels_ == 3)sample_resized.convertTo(sample_float, CV_32FC3);
+  else sample_resized.convertTo(sample_float, CV_32FC1);
+  cv::split(sample_float, *input_channels);
+  CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
+        == net_->input_blobs()[0]->cpu_data())
+    << "Input channels are not wrapping the input layer of the network.";
+}
+int main(int argc, char** argv){
+    caffe::GlobalInit(&argc,&argv);
+    string file_path=(string)argv[1];
+    string proto_cnn_file=(string)argv[2];
+    string model_cnn_file=(string)argv[3];
+	Classifier classifier(proto_cnn_file,model_cnn_file);
+    Mat image=imread(file_path);
+    Mat resizeimg;
+    resize(image,resizeimg,Size(128,32),0,0,CV_INTER_LINEAR);
+    vector<int> ans=classifier.Classify(resizeimg);
+    for(int i=0;i<ans.size();i++){
+        if(ans[i]==36)cout<<'-';
+        else cout<<ans[i];
+    }
+    cout<<endl;
+}
